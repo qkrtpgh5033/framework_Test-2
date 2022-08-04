386cafdab6ea7736ab0322fa078bba952f886eaa
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-08-04 13:04:56 +0900

    feat : write 기능 및 beforeEach를 통해 독립적 테스트 진행

--- src/test/java/com/ll/exam/ArticleServiceTest.java
+++ src/test/java/com/ll/exam/ArticleServiceTest.java
@@ -2,10 +2,13 @@ package com.ll.exam;
 
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.article.service.ArticleService;
+import com.ll.exam.mymap.MyMap;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +19,42 @@ public class ArticleServiceTest {
     static void beforeAll(){
         articleService = Container.getObj(ArticleService.class);
     }
+
+    @BeforeEach
+    public void beforeEach() {
+        // 게시물 테이블을 깔끔하게 삭제한다.
+        // DELETE FROM article; // 보다 TRUNCATE article; 로 삭제하는게 더 깔끔하고 흔적이 남지 않는다.
+        truncateArticleTable();
+        // 게시물 3개를 만든다.
+        // 테스트에 필요한 샘플데이터를 만든다고 보면 된다.
+        makeArticleTestData();
+    }
+
+    private void makeArticleTestData() {
+        MyMap myMap = Container.getObj(MyMap.class);
+
+        IntStream.rangeClosed(1, 3).forEach(no -> {
+            boolean isBlind = false;
+            String title = "제목%d".formatted(no);
+            String body = "내용%d".formatted(no);
+
+            myMap.run("""
+                    INSERT INTO article
+                    SET createdDate = NOW(),
+                    modifiedDate = NOW(),
+                    title = ?,
+                    `body` = ?,
+                    isBlind = ?
+                    """, title, body, isBlind);
+        });
+    }
+
+    private void truncateArticleTable() {
+        MyMap myMap = Container.getObj(MyMap.class);
+        // 테이블을 깔끔하게 지워준다.
+        myMap.run("TRUNCATE article");
+    }
+
     @Test
     public void articleService가_존재한다(){
         assertThat(articleService).isNotNull();
@@ -52,5 +91,20 @@ public class ArticleServiceTest {
         assertThat(articlesCount).isEqualTo(3);
     }
 
+    @Test
+    public void write() {
+        ArticleService articleService = Container.getObj(ArticleService.class);
+
+        long newArticleId = articleService.write("제목 new", "내용 new", false);
+
+        ArticleDto articleDto = articleService.getArticleById(newArticleId);
+
+        assertThat(articleDto.getId()).isEqualTo(newArticleId);
+        assertThat(articleDto.getTitle()).isEqualTo("제목 new");
+        assertThat(articleDto.getBody()).isEqualTo("내용 new");
+        assertThat(articleDto.getCreatedDate()).isNotNull();
+        assertThat(articleDto.getModifiedDate()).isNotNull();
+        assertThat(articleDto.isBlind()).isEqualTo(false);
+    }
 
 }
